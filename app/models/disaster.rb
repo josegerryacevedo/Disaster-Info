8cfebe4d74b6964f67ab8d85571e1a4bf7632045
josegerryacevedo <https://github.com/josegerryacevedo/acevedo-preclass-project.git>
2022-07-18 17:43:29 +0800

    Validates the important column data

--- app/models/disaster.rb
+++ app/models/disaster.rb
@@ -2,7 +2,6 @@ class Disaster < ApplicationRecord
   validates_presence_of :title
   validates_presence_of :content
   validates_presence_of :address
-  validates_presence_of :long_url
 
   belongs_to :user
 end
