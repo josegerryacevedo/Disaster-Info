e98eb2be7e0a1ea09cf1221839992fb57ef09b8f
josegerryacevedo <https://github.com/josegerryacevedo/acevedo-preclass-project.git>
2022-07-18 18:39:03 +0800

    Add Edit, Show & Delete function

--- app/controllers/disasters_controller.rb
+++ app/controllers/disasters_controller.rb
@@ -1,6 +1,7 @@
 class DisastersController < ApplicationController
   before_action :authenticate_user!
-
+  before_action :set_disaster, only: :show
+  before_action :set_own_disaster, only: [:edit, :update, :destroy]
 
   def index
     @disasters = Disaster.all.includes(:user)
@@ -20,10 +21,29 @@ class DisastersController < ApplicationController
     end
   end
 
+  def edit; end
+
+  def update
+
+    if @disaster.update(disaster_params)
+      redirect_to disasters_path
+    else
+      render :edit
+    end
+  end
+
+  def show; end
+
   def set_own_disaster
     @disaster = current_user.disasters.find_by_id(params[:id])
     if @disaster.nil?
       flash[:alert] = 'this post not belongs_to you or not exists'
+      redirect_to disasters_path
+    end
+  end
+
+  def destroy
+    if @disaster.destroy
       redirect_to disasters_path
     end
   end
