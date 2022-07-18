9cd965a0ec104ca4cd741d9fb5231e595bb8b267
josegerryacevedo <https://github.com/josegerryacevedo/acevedo-preclass-project.git>
2022-07-18 17:44:58 +0800

    Create funtions

--- app/controllers/disasters_controller.rb
+++ app/controllers/disasters_controller.rb
@@ -2,4 +2,40 @@ class DisastersController < ApplicationController
   before_action :authenticate_user!
 
 
+  def index
+    @disasters = Disaster.all.includes(:user)
+  end
+
+  def new
+    @disaster = Disaster.new
+  end
+
+  def create
+    @disaster = Disaster.new(disaster_params)
+    @disaster.user = current_user
+    if @disaster.save
+      redirect_to disasters_path
+    else
+      render :new
+    end
+  end
+
+  def set_own_disaster
+    @disaster = current_user.disasters.find_by_id(params[:id])
+    if @disaster.nil?
+      flash[:alert] = 'this post not belongs_to you or not exists'
+      redirect_to disasters_path
+    end
+  end
+
+  private
+
+  def disaster_params
+    params.require(:disaster).permit(:title, :content, :address )
+  end
+
+  def set_disaster
+    @disaster = Disaster.find(params[:id])
+  end
+
 end
