35ef437e7694a25cdba81c43861bec284d1a0129
josegerryacevedo <https://github.com/josegerryacevedo/acevedo-preclass-project.git>
2022-07-20 10:19:24 +0800

    Add relationship to type model

--- app/models/disaster.rb
+++ app/models/disaster.rb
@@ -2,7 +2,9 @@ class Disaster < ApplicationRecord
   validates_presence_of :title
   validates_presence_of :content
   validates_presence_of :address
+  validates_presence_of :type_id
 
   has_many :comments
   belongs_to :user
+  belongs_to :type
 end
