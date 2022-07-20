a5a763158ffd2291b823eff104e885f9d05cb7cc
josegerryacevedo <https://github.com/josegerryacevedo/acevedo-preclass-project.git>
2022-07-20 11:16:47 +0800

    Fix validates

--- app/models/disaster.rb
+++ app/models/disaster.rb
@@ -2,7 +2,6 @@ class Disaster < ApplicationRecord
   validates_presence_of :title
   validates_presence_of :content
   validates_presence_of :address
-  validates_presence_of :type_id
 
   has_many :comments
   belongs_to :user
