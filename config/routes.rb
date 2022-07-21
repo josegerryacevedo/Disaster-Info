813160db177d94b4f5ab23661879924ea78a03c6
josegerryacevedo <https://github.com/josegerryacevedo/acevedo-preclass-project.git>
2022-07-21 16:40:43 +0800

    Add 404 not found

--- config/routes.rb
+++ config/routes.rb
@@ -2,9 +2,10 @@ Rails.application.routes.draw do
   devise_for :users
   root :to => "disasters#index"
   resources :disasters do resources :comments end
-
   namespace :admin do
     resources :types
   end
+  get '*path', to: 'disasters#not_found'
+
 
 end
