dee2c536e0d13bde20546bc730d1e40049c4b24d
josegerryacevedo <https://github.com/josegerryacevedo/acevedo-preclass-project.git>
2022-07-26 14:20:05 +0800

    Add translate tagalog & english

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,4 +2,13 @@ class ApplicationController < ActionController::Base
   def not_found
     render :file => "#{Rails.root}/public/404.html", :status => 404, :layout => false
   end
+
+  before_action :set_locale
+
+  def set_locale
+    if params[:locale] && I18n.available_locales.include?( params[:locale].to_sym )
+      session[:locale] = params[:locale]
+    end
+    I18n.locale = session[:locale] || I18n.default_locale
+  end
 end
